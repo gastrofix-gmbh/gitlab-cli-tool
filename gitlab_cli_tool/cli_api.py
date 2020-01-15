48c7d033badf3ce253dd65f771f106359a844634
jakobowsky <jakob.wolitzki@gmail.com>
2020-01-15 12:14:36 +0100

    Deleted unused code

--- gitlab_cli_tool/cli_api.py
+++ gitlab_cli_tool/cli_api.py
@@ -15,14 +15,6 @@ class Filtering(Enum):
     WRONG = 0
     NAMES = 1
     TAGS = 2
-    # LIST = 1
-    # LIST_TAGS = 2
-    # LIST_NAMES = 3
-    # PAUSE_TAGS = 4
-    # PAUSE_NAMES = 5
-    # RESUME_TAGS = 6
-    # RESUME_NAMES = 7
-    # RUN_PIPELINE = 8
 
 
 class Actions(Enum):
@@ -105,7 +97,7 @@ class GitLabDataFilter:
 
     def filter_runners(self, current_runners, filter, filter_values):
         if filter == Filtering.NAMES:
-            return self.api.filter_by_names_new(current_runners, filter_values)
+            return self.api.filter_by_names_dict(current_runners, filter_values)
         elif filter == Filtering.TAGS:
             return self.api.get_projects_filtered_runners_by_tags(current_runners, filter_values)
 
@@ -152,82 +144,6 @@ class GitLabDataFilter:
             return self.make_action_on_runners(runners)
         elif self.property_name == PropertyName.PIPELINE.value:
             return self.api.run_pipeline(self.branch, self.project_id, self.variables)
-
-        # print(self.property_name)
-        # print(self.action)
-        # print(self.tags)
-        # print(self.names)
-        # print(self.branch)
-        # print(self.variables)
-        # print(self.ignore)
-        # return 1
-        # return None
-        # filters = self.check_filters()
-        # project_name = self.api.get_project(self.project_id).name
-        # runners = []
-        #
-        # if filters == Filtering.WRONG:
-        #     return 'Wrong arguments'
-        #
-        # elif filters == Filtering.RUN_PIPELINE:
-        #     return self.api.run_pipeline(self.branch, self.project_id, self.variables)
-        #
-        # elif filters == Filtering.LIST_TAGS:
-        #     runners = self.api.get_projects_runners(self.project_id)
-        #     runners = self.api.assign_tags_to_runners_asyncio(runners)
-        #     runners = self.api.get_projects_filtered_runners_by_tags(runners, self.tags)
-        #
-        # elif filters == Filtering.LIST_NAMES:
-        #     runners = self.api.get_projects_filtered_runners_by_name(self.project_id, self.names)
-        #     runners = self.api.assign_tags_to_runners_asyncio(runners)
-        #
-        # elif filters == Filtering.LIST:
-        #     runners = self.api.get_projects_runners(self.project_id)
-        #     runners = self.api.assign_tags_to_runners_asyncio(runners)
-        #
-        # elif filters == Filtering.PAUSE_TAGS:
-        #     runners = self.api.get_projects_runners(self.project_id)
-        #     runners = self.api.assign_tags_to_runners_asyncio(runners)
-        #     runners = self.api.get_projects_filtered_runners_by_tags(runners, self.tags)
-        #     runners = self.api.change_runners_dict_status(runners, False)
-        #
-        # elif filters == Filtering.PAUSE_NAMES:
-        #     runners = self.api.get_projects_filtered_runners_by_name(self.project_id, self.names)
-        #     runners = self.api.change_runners_class_status(runners, False)
-        #     runners = self.api.assign_tags_to_runners_asyncio(runners)
-        #
-        # elif filters == Filtering.RESUME_TAGS:
-        #     runners = self.api.get_projects_runners(self.project_id)
-        #     runners = self.api.assign_tags_to_runners_asyncio(runners)
-        #     runners = self.api.get_projects_filtered_runners_by_tags(runners, self.tags)
-        #     runners = self.api.change_runners_dict_status(runners, True)
-        #
-        # elif filters == Filtering.RESUME_NAMES:
-        #     runners = self.api.get_projects_filtered_runners_by_name(self.project_id, self.names)
-        #     runners = self.api.change_runners_class_status(runners, True)
-        #     runners = self.api.assign_tags_to_runners_asyncio(runners)
-        #
-        # runners = self.api.assign_active_jobs_to_runners(runners, self.project_id)
-        # return self.format_output(runners, project_name)
-
-    # def check_filters(self):
-    #     if self.property_name == PropertyName.RUNNERS.value and self.action == Actions.LIST.value and self.tags:
-    #         return Filtering.LIST_TAGS
-    #     elif self.property_name == PropertyName.RUNNERS.value and self.action == Actions.LIST.value and self.names:
-    #         return Filtering.LIST_NAMES
-    #     elif self.property_name == PropertyName.RUNNERS.value and self.action == Actions.LIST.value:
-    #         return Filtering.LIST
-    #     elif self.property_name == PropertyName.RUNNERS.value and self.action == Actions.PAUSE.value and self.tags:
-    #         return Filtering.PAUSE_TAGS
-    #     elif self.property_name == PropertyName.RUNNERS.value and self.action == Actions.PAUSE.value and self.names:
-    #         return Filtering.PAUSE_NAMES
-    #     elif self.property_name == PropertyName.RUNNERS.value and self.action == Actions.RESUME.value and self.tags:
-    #         return Filtering.RESUME_TAGS
-    #     elif self.property_name == PropertyName.RUNNERS.value and self.action == Actions.RESUME.value and self.names:
-    #         return Filtering.RESUME_NAMES
-    #     elif self.property_name == PropertyName.PIPELINE.value and self.action == Actions.RUN.value and self.branch:
-    #         return Filtering.RUN_PIPELINE
-    #     return Filtering.WRONG
 
 
 class GitlabAPI:
@@ -303,7 +219,7 @@ class GitlabAPI:
         return list(set([runner.id for runner in filtered_runners]))
 
     @staticmethod
-    def filter_by_names_new(runners: List[dict], names: List[str]) -> List[dict]:
+    def filter_by_names_dict(runners: List[dict], names: List[str]) -> List[dict]:
         if not isinstance(names, list):
             raise RuntimeError(f'"names" must be a list, {type(names)} was passed!')
         filtered_runners = []
@@ -312,7 +228,6 @@ class GitlabAPI:
                 if name.lower() in runner['description'].lower():
                     filtered_runners.append(runner)
                     break
-        # return list(set([runner.id for runner in filtered_runners]))
         return filtered_runners
 
     def get_projects_filtered_runners_by_name(self, project_id, names):
@@ -412,33 +327,6 @@ class GitlabAPI:
             else:
                 counted_jobs[job['runner']['id']] = 1
         return counted_jobs
-
-    def change_runners_class_status(self, runners: List[ProjectRunner], status: bool) -> List[ProjectRunner]:
-        """
-        Function which pauses or resumes all selected runners
-        :param runners: List of runners [ProjectRunner]
-        :param status: True (Resume) / False (Pause)
-        :return: List of runners [ProjectRunner]
-        """
-        payload = {'active': status}
-        for runner in runners:
-            try:
-                url = f'{self.server}/api/v4/runners/{runner.id}'
-                response = requests.put(url, headers=self.headers, data=payload)
-                response.raise_for_status()
-                if status:
-                    runner._attrs['status'] = 'online'
-                    print(f'Runner id: {runner.id} is resumed')
-                else:
-                    runner._attrs['status'] = 'paused'
-                    print(f'Runner id: {runner.id} is paused')
-            except Exception as err:
-                if status:
-                    print(f'Runner {runner.id} cannot be resumed because of {err}')
-                else:
-                    print(f'Runner {runner.id} cannot be paused because of {err}')
-
-        return runners
 
     def change_runners_dict_status(self, runners: List[dict], status: bool) -> List[dict]:
         """
